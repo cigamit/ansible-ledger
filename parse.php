38086056577efdc3ae53fee45cce171f8a5e8535
Jimmy Conner <jimmy@sqmail.org>
2022-09-28 01:02:44 -0500

    Add color diffs to change output

--- parse.php
+++ parse.php
@@ -1,7 +1,12 @@
 <?php
 
 include_once('includes/classes/Spyc.php');
+
 use Async\Yaml;
+
+include_once('vendor/autoload.php');
+use SebastianBergmann\Diff\Differ;
+use SebastianBergmann\Diff\Output\StrictUnifiedDiffOutputBuilder;
 
 
 $d = file_get_contents('php://input');
@@ -32,17 +37,62 @@ if (isset($d['logger_name'])) {
 				}
 			}
 
-			if (isset($d['changed']) && $d['changed'] && isset($d['job']) && $d['job'] && isset($d['event_data']['task_action']) && $d['event_data']['task_action'] != '') {
+			if (isset($d['changed']) && $d['changed'] && isset($d['event']) && $d['event'] == 'runner_on_ok' && isset($d['job']) && $d['job'] && isset($d['event_data']['task_action']) && $d['event_data']['task_action'] != '') {
 				include_once('includes/sql.php');
+		//file_put_contents('events.txt', print_r($d, true), FILE_APPEND);
+
+				if (isset($d['event_data']['res']['results'])) {
+					for ($r = 0; $r < count($d['event_data']['res']['results']); $r++) {
+
+						for ($a = 0; $a < count($d['event_data']['res']['results'][$r]['diff']); $a++) {
+							if (isset($d['event_data']['res']['results'][$r]['diff'][$a]['before']) && isset($d['event_data']['res']['results'][$r]['diff'][$a]['after'])) {
+								$from = $to = (isset($d['event_data']['res']['results'][$r]['diff'][1]['before_header']) ? $d['event_data']['res']['results'][$r]['diff'][0]['before_header'] : null);
+
+								$builder = new StrictUnifiedDiffOutputBuilder([
+									'collapseRanges'      => true, // ranges of length one are rendered with the trailing `,1`
+									'commonLineThreshold' => 6,    // number of same lines before ending a new hunk and creating a new one (if needed)
+									'contextLines'        => 3,    // like `diff:  -u, -U NUM, --unified[=NUM]`, for patch/git apply compatibility best to keep at least @ 3
+									'fromFile'            => $from,
+									'fromFileDate'        => null,
+									'toFile'              => $to,
+									'toFileDate'          => null,
+								]);
+
+								$differ = new Differ($builder);
+								$diff = $differ->diff($d['event_data']['res']['results'][$r]['diff'][0]['before'], $d['event_data']['res']['results'][$r]['diff'][0]['after']);
+								$d['event_data']['res']['results'][$r]['diff'][$a] = color_diff($diff);
+							}
+						}
+					}
+				}
+
+				if (isset($d['event_data']['res']['diff'])) {
+					for ($a = 0; $a < count($d['event_data']['res']['diff']); $a++) {
+						if (isset($d['event_data']['res']['diff'][$a]['before']) && isset($d['event_data']['res']['diff'][$a]['after'])) {
+							$from = $to = (isset($d['event_data']['res']['diff'][1]['before_header']) ? $d['event_data']['res']['diff'][0]['before_header'] : null);
+
+							$builder = new StrictUnifiedDiffOutputBuilder([
+								'collapseRanges'      => true, // ranges of length one are rendered with the trailing `,1`
+								'commonLineThreshold' => 6,    // number of same lines before ending a new hunk and creating a new one (if needed)
+								'contextLines'        => 3,    // like `diff:  -u, -U NUM, --unified[=NUM]`, for patch/git apply compatibility best to keep at least @ 3
+								'fromFile'            => $from,
+								'fromFileDate'        => null,
+								'toFile'              => $to,
+								'toFileDate'          => null,
+							]);
+
+							$differ = new Differ($builder);
+							$diff = $differ->diff($d['event_data']['res']['diff'][0]['before'], $d['event_data']['res']['diff'][0]['after']);
+							$d['event_data']['res']['diff'][$a] = color_diff($diff);
+						}
+					}
+				}
 
 				$h = check_host($d['host_name']);
 				$role = (isset($d['role']) ? $d['role'] : '');
 				$res = Yaml::dumper($d['event_data']['res']);
 				db_execute_prepare('INSERT INTO `changes` (`host`, `time`, `job`, `playbook`, `play`, `role`, `task`, `task_action`, `res`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
 					array($h, time(), $d['job'], $d['playbook'], $d['play'], $role, $d['task'], $d['event_data']['task_action'], $res));
-
-//file_put_contents('events.txt', print_r($d, true), FILE_APPEND);
-
 			}
 			break;
 		case 'awx.analytics.activity_stream':
@@ -62,6 +112,24 @@ if (isset($d['logger_name'])) {
 			}
 			break;
 	}
+}
+
+function color_diff($diff) {
+	$diff = explode("\n", $diff);
+	for ($a = 0; $a < count($diff); $a++) {
+		if (isset($diff[$a][0])) {
+			if ($diff[$a][0] == '+') {
+				$diff[$a] = '<font color=green>' . $diff[$a] . '</font>';
+			}
+			if ($diff[$a][0] == '-') {
+				$diff[$a] = '<font color=red>' . $diff[$a] . '</font>';
+			}
+			if ($diff[$a][0] == '@') {
+				$diff[$a] = '<font color=blue>' . $diff[$a] . '</font>';
+			}
+		}
+	}
+	return implode("\n", $diff);
 }
 
 function parse_facts ($f, $fs = array(), $n = '') {
