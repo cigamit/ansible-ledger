5f04ab9c511efe110517d4cadc3acb301a4a4b41
Jimmy Conner <jimmy@sqmail.org>
2022-10-05 22:13:23 -0500

    Catch errors on upgrade

--- includes/db_upgrade.php
+++ includes/db_upgrade.php
@@ -9,11 +9,15 @@ if ($db_version == '') {
 }
 
 if ($new_db_version != $db_version) {
-	switch ($db_version) {
-		case 2:
-			@db_execute('ALTER TABLE `users` ADD `super` tinyint(1) NOT NULL DEFAULT '0';');
+	try {
+		switch ($db_version) {
+			case 2:
+				db_execute("ALTER TABLE `users` ADD `super` tinyint(1) NOT NULL DEFAULT '0';");
+
+
+		}
+	} catch (Exception $e) {
 
 	}
-
 	db_execute_prepare("UPDATE `settings` SET `value` = ? WHERE `setting` = 'db_version'", array($new_db_version));
 }
